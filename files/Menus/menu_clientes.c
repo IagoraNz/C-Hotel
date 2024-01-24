b528d5a8a3c8739e8e4b46540cef98d767292d11
FrancinaldoSB <francinaldosousabarbosa21@gmail.com>
2024-01-24 20:09:53 -0300

    atts menu_clientes

--- files/Menus/menu_clientes.c
+++ files/Menus/menu_clientes.c
@@ -7,21 +7,25 @@
 #include "../Cliente/consultar_cliente.c"
 #include "../Cliente/edit_cliente.c"
 
-
-void menuClientes(){
-    Clientes cliente;
+void menuClientes()
+{
+    Clientes cliente1;
     int opc;
-    FILE *arq;
+    FILE *cliente;
 
-    arq = fopen("..\\db\\clientes.txt", "r");
+    cliente = fopen("..\\db\\cliente.txt", "r");
 
     int cont = 0;
 
-    while(fread(&cliente, sizeof(Clientes), 1, arq)){
+    while (fscanf(cliente, "%s %d %03d.%03d.%03d-%02d %d %s %s %s %s\n", cliente1.nome, &cliente1.idade,
+                  &cliente1.bloco1, &cliente1.bloco2, &cliente1.bloco3, &cliente1.bloco4,
+                  &cliente1.rg, cliente1.email, cliente1.telefone, cliente1.cidade, cliente1.estado) == 11)
+    {
         cont++;
     }
-
-    do{
+    fclose(cliente);
+    do
+    {
         printf("\xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBB\n");
         printf("\xBA       MENU CLIENTES      \xBA\n");
         printf("\xC8\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBC\n");
@@ -33,40 +37,47 @@ void menuClientes(){
         printf("Digite a opcao desejada: ");
         scanf("%d", &opc);
 
-        switch (opc) {
+        switch (opc)
+        {
         case 1:
             Cadastro_Cliente(&cliente);
             break;
         case 2:
-            if(cont == 0){
+            if (cont == 0)
+            {
                 printf("Nenhum cliente cadastrado!\n");
                 system("PAUSE");
                 system("cls");
                 break;
             }
-            else{
+            else
+            {
                 Listar_Clientes();
             }
             break;
         case 3:
-            if(cont == 0){
+            if (cont == 0)
+            {
                 printf("Nenhum cliente cadastrado!\n");
                 system("PAUSE");
                 system("cls");
                 break;
             }
-            else{
+            else
+            {
                 Consultar_Cliente();
             }
             break;
         case 4:
-            if(cont == 0){
+            if (cont == 0)
+            {
                 printf("Nenhum cliente cadastrado!\n");
                 system("PAUSE");
                 system("cls");
                 break;
             }
-            else{
+            else
+            {
                 Editar_Cliente();
             }
             break;
@@ -75,7 +86,5 @@ void menuClientes(){
         default:
             printf("Opcao invalida. Tente novamente.\n");
         }
-    }while(opc != 0);
-
-    fclose(arq);
+    } while (opc != 0);
 }
